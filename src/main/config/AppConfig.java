3bed1ccd09c1ce6fa1086b01927a4c0ce9f37d75
Sergei Murkou <murkous@mail.ru>
2017-12-02 14:47:48 +0300

    add static resources

--- src/main/config/AppConfig.java
+++ src/main/config/AppConfig.java
@@ -3,10 +3,17 @@ package main.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "main")
-public class AppConfig {
-
+public class AppConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry resourceHandlerRegistry) {
+        resourceHandlerRegistry
+                .addResourceHandler("/**")
+                .addResourceLocations("/");
+    }
 }
