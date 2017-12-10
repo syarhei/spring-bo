455c2b326fad1faf67b0ff438941147650467751
Sergei Murkou <murkous@mail.ru>
2017-12-10 18:30:56 +0300

    add generation of coefficients

--- src/main/controllers/MatchController.java
+++ src/main/controllers/MatchController.java
@@ -1,7 +1,9 @@
 package main.controllers;
 
 import main.models.Match;
+import main.models.Team;
 import main.services.MatchService;
+import main.services.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,19 @@ import org.springframework.web.bind.annotation.*;
 public class MatchController extends Controller<Match> {
     @Autowired
     private MatchService matchService;
+
+    @Autowired
+    private TeamService teamService;
+
+    @Override
+    public ResponseEntity createEntity(@RequestBody Match object) {
+        Team team1 = teamService.getById(object.getTeam1().getId());
+        Team team2 = teamService.getById(object.getTeam2().getId());
+
+        matchService.generateCoefficients(object, team1, team2);
+
+        return super.createEntity(object);
+    }
 
     @GetMapping("/{primaryKey}")
     public ResponseEntity getEntity(@PathVariable Integer primaryKey) {
--- src/main/services/MatchService.java
+++ src/main/services/MatchService.java
@@ -1,14 +1,67 @@
 package main.services;
 
 import main.models.Match;
+import main.models.Team;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @Repository
 @Transactional
 public class MatchService extends Service<Match> {
     public MatchService() {
         super(Match.class);
+    }
+
+    public void generateCoefficients(Match match, Team team1, Team team2) {
+        Integer games1 = team1.getGames();
+        Integer games2 = team2.getGames();
+
+        if (games1 == 0 || games2 == 0) {
+            setDefaultCoefficients(match);
+        }
+
+        else {
+            Double rating1 = team1.getPoints().doubleValue() / games1;
+            Double rating2 = team2.getPoints().doubleValue() / games2;
+
+            if (rating1 > rating2) {
+                setCoefficients(match, true);
+            } else {
+                setCoefficients(match, false);
+            }
+        }
+    }
+
+    private void setDefaultCoefficients(Match match) {
+        BigDecimal win1 = new BigDecimal(Math.random()/4 + 1.5);
+        win1 = win1.setScale(2, RoundingMode.HALF_UP);
+
+        BigDecimal win2 = new BigDecimal(Math.random()/4 + 1.5);
+        win2 = win2.setScale(2, RoundingMode.HALF_UP);
+
+        BigDecimal draw = new BigDecimal(Math.random()/2 + 1.5);
+        draw = draw.setScale(2, RoundingMode.HALF_UP);
+
+        match.setCoefficientWin1(win1);
+        match.setCoefficientWin2(win2);
+        match.setCoefficientDraw(draw);
+    }
+
+    private void setCoefficients(Match match, boolean isFirst) {
+        BigDecimal win1 = new BigDecimal(Math.random()/4 + 1.5);
+        win1 = win1.setScale(2, RoundingMode.HALF_UP);
+
+        BigDecimal win2 = new BigDecimal(Math.random() + 2.5);
+        win2 = win2.setScale(2, RoundingMode.HALF_UP);
+
+        BigDecimal draw = new BigDecimal(Math.random()/2 + 1.75);
+        draw = draw.setScale(2, RoundingMode.HALF_UP);
+
+        match.setCoefficientWin1(isFirst ? win1 : win2);
+        match.setCoefficientWin2(isFirst ? win2: win1);
+        match.setCoefficientDraw(draw);
     }
 }
