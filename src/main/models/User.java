c6f009cedf5a3e77476e1e21044efa7e6792e5fc
Sergei Murkou <murkous@mail.ru>
2017-12-14 01:52:45 +0300

    json ignore password

--- src/main/models/User.java
+++ src/main/models/User.java
@@ -1,5 +1,9 @@
 package main.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import javax.persistence.*;
 
 @Entity
@@ -10,9 +14,11 @@ public class User {
     private String nickname;
 
     // TODO: BCrypt password (hash length = 60)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @Column(nullable = false, length = 60)
     private String password;
 
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @Column(length = 5, nullable = false, updatable = false)
     private String role = "user";
 
